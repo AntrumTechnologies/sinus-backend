4f612667b99a5ed0fa0b3abca2af4ef467dff55a
Patrick van Broeckhuijsen <github@patb.ro>
2022-09-10 17:49:14 +0200

    Add ability to delete sinus value

--- app/Http/Controllers/API/SinusValueController.php
+++ app/Http/Controllers/API/SinusValueController.php
@@ -33,4 +33,14 @@ class SinusValueController extends Controller
         $sinusValues = $sinusValues->makeHidden(['id', 'sinus_id', 'created_at', 'updated_at']);
 		return response()->json($sinusValues);
 	}
+
+	public function delete(Request $request) {
+		$request->validate([
+			'sinus_id' => 'required|integer',
+			'date' => 'required|date',
+		]);
+
+		$sinusValues = SinusValue::where('sinus_id', $request->get('sinus_id'))->where('date', $request->get('date'))->delete();
+		return response()->json($sinusValues);
+	}	
 }
