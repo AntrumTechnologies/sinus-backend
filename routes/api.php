8088e1e6df869ee007cc06386db2c9a80eaba1dd
Patrick van Broeckhuijsen <pvanbroeckhuijsen@gmail.com>
2022-09-10 11:58:32 +0200

    Add API routes for Sinus and SinusValue controllers

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,9 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\API\SinusController;
+use App\Http\Controllers\API\SinusValueController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,3 +20,10 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('sinus', [SinusController::class, 'index']);
+Route::get('sinus/{id}', [SinusController::class, 'show']);
+Route::put('sinus', [SinusController::class, 'store']);
+
+Route::get('sinusvalue/{id}', [SinusValueController::class, 'show']);
+Route::put('sinusvalue', [SinusValueController::class, 'store']);
