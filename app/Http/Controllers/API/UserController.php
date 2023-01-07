ac7dca734d1c2d135db310049afda526f0a8577f
Patrick van Broeckhuijsen <pvanbroeckhuijsen@gmail.com>
2023-01-07 18:51:59 +0100

    Add used facades and update filesystem config

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -8,6 +8,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Log;
 
 class UserController extends Controller
 {
