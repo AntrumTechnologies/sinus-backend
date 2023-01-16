b83fc92504e47b372c995696d08ce240f9a4423b
Patrick van Broeckhuijsen <github@patb.ro>
2023-01-16 22:21:16 +0100

    Add AlwaysTo email and add verify email without login

---
+++ app/Http/Controllers/VerificationController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Auth\Events\Verified;
+use Illuminate\Http\Request;
+
+class VerificationController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth')->except('verify');
+        $this->middleware('signed')->only('verify');
+        $this->middleware('throttle:6,1')->only('verify', 'resend');
+    }
+
+    public function verify(Request $request)
+    {
+        $user = User::find($request->route('id'));
+        if ($user->email_verified_at) {
+            return true;
+        }
+
+        if (!hash_equals((string) $request->route('hash'), sha1($user->getEmailForVerification()))) {
+            throw new AuthorizationException;
+        }
+
+        if ($user->markEmailAsVerified())
+            event(new Verified($user));
+
+        return true;
+    }
+}
